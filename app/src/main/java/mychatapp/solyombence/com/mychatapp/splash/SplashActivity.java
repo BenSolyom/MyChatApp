a259d59f0f65ae225de204857f405612dedf7a3f
BenSolyom <solyombence@freemail.hu>
2019-09-05 21:46:47 +0200

    Code clean up in a few XML files and the Login and Splash activities. Added fade in/fade out animation between the Login and Splash activities. Added some icon PNGs for the chatroom activity.

--- app/src/main/java/mychatapp/solyombence/com/mychatapp/splash/SplashActivity.java
+++ app/src/main/java/mychatapp/solyombence/com/mychatapp/splash/SplashActivity.java
@@ -5,32 +5,34 @@ import mychatapp.solyombence.com.mychatapp.R;
 import mychatapp.solyombence.com.mychatapp.lobby.ChatLobbyActivity;
 import mychatapp.solyombence.com.mychatapp.login.LoginActivity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashActivity extends AppCompatActivity {
 
-    private SharedPreferences sharedPreferences;
     private static int SPLASH_TIME_OUT = 1000;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        sharedPreferences = getSharedPreferences("sharedPref", MODE_PRIVATE);
+        auth = FirebaseAuth.getInstance();
 
         new Handler().postDelayed(new Runnable() {
         @Override
         public void run() {
             Intent homeIntent;
-            if (sharedPreferences.getBoolean("login", false)) {
+            if (auth.getCurrentUser() != null) {
                 homeIntent = new Intent(SplashActivity.this, ChatLobbyActivity.class);
             } else {
                 homeIntent = new Intent(SplashActivity.this, LoginActivity.class);
             }
             startActivity(homeIntent);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             finish();
             }
         }, SPLASH_TIME_OUT);
